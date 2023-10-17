6c57cc1d2f165521e537c2cb024a346bdf0592e6
root <root@DESKTOP-O7CMANF>
2023-10-17 18:40:10 -0300

    remove navbar

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,11 @@ import PagSchool from './components/42school/PagSchool';
 import PagInitial from './components/PageInitial/PageInitial';
 import Perfil from './components/Perfil/Perfil';
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
-// import NavBar from './components/navBar/NavBar';
 
 function App() {
 	return (
 		<div className="App">
 			<BrowserRouter>
-				{/* <NavBar /> */}
 				<Perfil />
 				<Routes>
 					<Route path="/" element={<PagInitial />} />
